fd67ce4723fff13e3202d823b440d89d0b7b1b50
Reid Efford <reidefford@gmail.com>
2022-12-06 11:01:59 -0500

    rmv dev config

--- src/Sf-JSON.svelte
+++ src/Sf-JSON.svelte
@@ -1,22 +1,21 @@
 <script>
   import Item from './lib/Item.svelte'
-  import { sampleData } from './dev/dev'
+  // import { sampleData } from './dev/dev'
 
-  export let data 
+  export let data = []
   export let labelMap = new Map()
-  export let keysToNotDisplay = [
-    'totalSize',
-    'done',
-    'rowLoadDate',
-    'attributes',
-    'totalSize'
-  ]
+  export let keysToNotDisplay = []
+    // 'totalSize',
+    // 'done',
+    // 'rowLoadDate',
+    // 'attributes',
+    // 'totalSize'
 
-  labelMap.set('ID', 'Id')
-  labelMap.set('account_case', 'Account Case')
-  labelMap.set('RecordType', 'Record Type')
+  // labelMap.set('ID', 'Id')
+  // labelMap.set('account_case', 'Account Case')
+  // labelMap.set('RecordType', 'Record Type')
 
-  data = typeof(data) === 'string' ? JSON.parse(sampleData) : sampleData 
+  data = typeof(data) === 'string' ? JSON.parse(data) : data 
   
 </script>
 
