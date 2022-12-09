e52ac97d32317e79e47520b01ed9f5f051879d3a
Reid Efford <reidefford@gmail.com>
2022-12-09 10:48:00 -0500

    clean up for prod

--- src/Sf-JSON.svelte
+++ src/Sf-JSON.svelte
@@ -1,23 +1,23 @@
 <script>
   import Item from './lib/Item.svelte'
-  import { sampleData } from './dev/dev'
+  // import { sampleData } from './dev/dev'
 
   export let data = []
   export let labelMap = new Map()
   export let keysToNotDisplay = [
-    'totalSize',
-    'done',
-    'rowLoadDate',
-    'attributes',
-    'totalSize'
+    // 'totalSize',
+    // 'done',
+    // 'rowLoadDate',
+    // 'attributes',
+    // 'totalSize'
   ]
 
   // labelMap.set('ID', 'Id')
   // labelMap.set('account_case', 'Account Case')
   // labelMap.set('RecordType', 'Record Type')
 
-  data = typeof(sampleData) === 'string' ? JSON.parse(sampleData) : sampleData 
-  // data = typeof(data) === 'string' ? JSON.parse(data) : data 
+  // data = typeof(sampleData) === 'string' ? JSON.parse(sampleData) : sampleData 
+  data = typeof(data) === 'string' ? JSON.parse(data) : data 
   
 </script>
 
